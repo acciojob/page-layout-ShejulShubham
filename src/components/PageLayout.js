0bec8fc35ffb5a24258eed21286e829aa34f2fa5
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-11-25 11:19:36 +0530

    finally done

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,13 +1,11 @@
-
-
-export default function PageLayout({Header, Footer, Children}){
-
-
-    return (
-        <>
-        <Header />
-        <Children />
-        <Footer />
-        </>
-    )
+import React from "react";
+// PageLayout.jsx
+export default function PageLayout({ header, footer, children }) {
+    return (
+        <div>
+            <header>{header}</header>
+            <main>{children}</main>
+            <footer>{footer}</footer>
+        </div>
+    );
 }
